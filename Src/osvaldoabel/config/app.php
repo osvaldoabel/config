95ec2d3cc4e6aac5abdff2e73ccb98c09402bbc9
Osvaldo Abel <omya01.01.1992@gmail.com>
2016-10-21 00:09:10 -0200

    Create a Config File

---
+++ Src/osvaldoabel/config/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return  [
+  'fruits' => [
+    'item' => [
+      'id' => 121545154,
+      'name'  => 'banana',
+      'price' => 100
+    ],
+    'item' => [
+      'id' => 121545154,
+      'name'  => 'Orange',
+      'price' => 45
+    ]
+  ],
+  'drinks' => [
+    'item' => [
+      'id' => 121545154,
+      'name'  => 'coca cola',
+      'price' => 4.50
+    ],
+    'item' => [
+      'id' => 5454545,
+      'name'  => 'Sprite',
+      'price' => 4.99
+    ]
+  ]
+];
