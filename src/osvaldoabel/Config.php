36b811e73fb7358445ba32faabe4855301cb22de
Osvaldo Abel <omya01.01.1992@gmail.com>
2016-10-22 08:51:56 -0200

    [FIX] - Improve the logic of  get method

--- src/osvaldoabel/Config.php
+++ src/osvaldoabel/Config.php
@@ -7,33 +7,46 @@ class Config
   protected $config_path;
   public function __construct()
   {
-    echo __DIR__;exit;
+    $this->config_path = __DIR__. '/config/';
+  }
+  /**
+   * @param Array $keys [Array of keys]
+   * @param Array (Optional) $array_content
+   * @return
+   */
+  public function getArrayContent($keys, $array_content = null)
+  {
+    foreach($keys as $key) {
+      if (!array_key_exists($key, $array_content)) {
+        return null;
+      }
+      //Gets the right position to Remove from array
+      $pos = array_search($key, $keys);
+      //Removes the current keys from next search
+      unset($keys[$pos]);
+      //
+      if(!empty($keys)) {
+        return $this->getArrayContent($keys, $array_content[$key]);
+      }
 
+      return $array_content[$key];
+    }
   }
 
-  public static function get($keys, $array = null)
+  public function get($keys)
   {
-    if(is_null($array)) {
-      $arr = explode('.', $keys);
-      $file = $arr[0].'.php';
-      unset($arr[0]);
+      $keys = explode('.', $keys);
 
+      $file = $this->config_path.$keys[0].'.php';
       if(!file_exists($file)) {
-        echo "error... this file does not exists";
+        return "error... this file does not exists";
         //
       }
-    }
+      //Removes filename from array
+      unset($keys[0]);
 
-    $fileContent  =  self::readFile($file);
-    //Removing the file name
-
-    foreach($arr as $key => $value) {
-      print_r($arr); exit;
-    }
-
-    $config = eval('return $fileContent'."$array;");
-    return $config;
-
+      $array  =  self::readFile($file);
+      return $this->getArrayContent($keys, $array);
   }
 
   public static function readFile($fileName)
