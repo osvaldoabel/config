838d78fe2d82fb334fb04fc743679f78e063e334
Osvaldo Abel <omya01.01.1992@gmail.com>
2016-10-21 23:54:06 -0200

    [fix] - Removing old GET funtion

--- src/osvaldoabel/Config.php
+++ src/osvaldoabel/Config.php
@@ -11,30 +11,6 @@ class Config
 
   }
 
-  /**@param String $keys [Receives an Array of keys]
-   *
-   */
-  // public static function @get($keys)
-  // {
-  //   $arr = explode('.', $keys);
-  //   $file = $arr[0].'.php';
-  //   //Checks if a File Exists
-  //   if(file_exists($file))
-  //   {
-  //     $fileContent =  self::readFile($file);
-  //     //Removing the file name
-  //     unset($arr[0]);
-  //
-  //     $array = '';
-  //     foreach($arr as $key ) {
-  //         $array .= "['$key']";
-  //     }
-  //
-  //     $config = eval('return $fileContent'."$array;");
-  //     return $config;
-  //   }
-  // }
-
   public static function get($keys, $array = null)
   {
     if(is_null($array)) {
